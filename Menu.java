42c3f39f6c1056c477c4d3c69505878d147f2a45
LeoRami99 <31749560+LeoRami99@users.noreply.github.com>
2018-10-29 20:31:10 -0500

    Color en el menú
    Se introducio color a las letras del menú por estetica de la calculadora

--- Menu.java
+++ Menu.java
@@ -1,3 +1,5 @@
+//calculadora desarrollada por Juan Leonardo Ramírez Velásquez.
+//Se es modificable el código a su gusto.
 import java.util.*;
 
 class Menu 
@@ -12,25 +14,25 @@ class Menu
 		System.out.println("Escribe tú nombre");
 		name1 = tc.nextLine();
 		System.out.println("\n");
-		System.out.println("\t---Hola "+ nombre(name1)+"---");
+		System.out.println("\t\033[32m---Hola "+ nombre(name1)+"---");
 		System.out.println("\n");
 	
 	while (!salir) 
 		{
 			System.out.println("*******************************************************");                                                                                                                                                                                      
-			System.out.println("▒█▀▀█ █▀▀█ █░░ █▀▀ █░░█ █░░ █▀▀█ █▀▀▄ █▀▀█ █▀▀█ █▀▀█ ");
-			System.out.println("▒█░░░ █▄▄█ █░░ █░░ █░░█ █░░ █▄▄█ █░░█ █░░█ █▄▄▀ █▄▄█ ");
-			System.out.println("▒█▄▄█ ▀░░▀ ▀▀▀ ▀▀▀ ░▀▀▀ ▀▀▀ ▀░░▀ ▀▀▀░ ▀▀▀▀ ▀░▀▀ ▀░░▀ ");
+			System.out.println("\033[31m▒█▀▀█ █▀▀█ █░░ █▀▀ █░░█ █░░ █▀▀█ █▀▀▄ █▀▀█ █▀▀█ █▀▀█ ");
+			System.out.println("\033[31m▒█░░░ █▄▄█ █░░ █░░ █░░█ █░░ █▄▄█ █░░█ █░░█ █▄▄▀ █▄▄█ ");
+			System.out.println("\033[31m▒█▄▄█ ▀░░▀ ▀▀▀ ▀▀▀ ░▀▀▀ ▀▀▀ ▀░░▀ ▀▀▀░ ▀▀▀▀ ▀░▀▀ ▀░░▀ ");
 			System.out.println("*******************************************************");
-			System.out.println("\tOpci\u00f3n 1 Sumar");
-			System.out.println("\tOpci\u00f3n 2 Restar");
-			System.out.println("\tOpci\u00f3n 3 Multiplicar");
-			System.out.println("\tOpci\u00f3n 4 Dividir");
-			System.out.println("\tOpci\u00f3n 5 Salir");
+			System.out.println("\t\033[31m*Opci\u00f3n 1 Sumar");
+			System.out.println("\t\033[32m*Opci\u00f3n 2 Restar");
+			System.out.println("\t\033[33m*Opci\u00f3n 3 Multiplicar");
+			System.out.println("\t\033[34m*Opci\u00f3n 4 Dividir");
+			System.out.println("\t\033[35m*Opci\u00f3n 5 Salir");
 			
 			try
 			{
-				System.out.println("Digita una de las opciones");
+				System.out.println("\033[37mDigita una de las opciones");
 				System.out.print(">>");
 				opcion = tc.nextInt();
 				switch (opcion) 
@@ -38,14 +40,14 @@ class Menu
 					case 1:
 						double suma=0,resum=0;
 						System.out.println("***********************************************************");
-						System.out.println("\t"+nombre(name1)+" estas en el apartado de suma");
-						System.out.println("\tTe recordamos que no es necesario introducir el operador");
+						System.out.println("\t\033[37m"+nombre(name1)+" estas en el apartado de suma");
+						System.out.println("\t\033[37mTe recordamos que no es necesario introducir el operador");
 						System.out.println("***********************************************************");
 						int i=0;
 						do
 						{	
-							System.out.println("Ingresa el " + (i+1) +" número");
-							System.out.print(">>");
+							System.out.println("\033[37mIngresa el " + (i+1) +" número");
+							System.out.print("\033[31m>>");
 							suma = tc.nextDouble();
 							resum=suma+resum;
 							i++;
@@ -70,12 +72,12 @@ class Menu
 						salir = true;
 					break;
 					default: 
-					System.out.println("Solo números entre 1 y 4");
+					System.out.println("\033[31mSolo números entre 1 y 4");
 				}
 			}
 			catch(InputMismatchException e)
 			{
-				System.out.println("Debe insertar un número valido...");
+				System.out.println("\033[31mDebe insertar un número valido...");
 				tc.next();
 			}
 		}
@@ -87,6 +89,6 @@ class Menu
 	public static void salida()
 	{
 		System.out.println("Gracias por usar nuestra calculadora");
-		System.out.println("Recuerda que puedes encontrar el codigo en: https://github.com/LeoRami99/Calculadora-.git");
+		System.out.println("Recuerda que puedes encontrar el codigo en: \033[31mhttps://github.com/LeoRami99/Calculadora-.git");
 	}
 }
