60d180913bd29402c2603a774a7b6fb0640ebf82
Leonardo Ramírez <31749560+LeoRami99@users.noreply.github.com>
2018-11-20 10:07:01 -0500

    Update and rename darwing.java to Ejercicio1.java

--- Ejercicio1.java
+++ Ejercicio1.java
@@ -1,7 +1,8 @@
 //Darwing Camilo Bustos Beltran
-public class darwing{
+public class Ejercicio1{
 	public static void main(String[] args){
-		String arrays[]={"4","7","11","4","9","5","11","7","3","5"};
+		String arrays[]={"4","7","11","4","9","5","11","7","3","5"};//se crea la variable string el cual cuenta con un array de los números propuestos en el PDF
+		//Se crea la condición de eliminar los números duplicados con while y dentro de este un if
 		int i=0;
 		while(i<arrays.length){
 			int j=0;
@@ -17,6 +18,7 @@ public class darwing{
 			}
 			i++;
 		}
+		//Se imprime en pantalla los números no duplicados
 	int x=arrays.length;
 	int y=0;
 		while (y<=x-1){
